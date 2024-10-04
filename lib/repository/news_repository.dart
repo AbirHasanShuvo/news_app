e6a39ed86bb64d6fa9e70a97368fa75f088e34fa
Md. Abir Hasan <abirhasanshuvo789@gmail.com>
2024-10-04 23:02:21 +0600

    API initialize

---
+++ lib/repository/news_repository.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:news_app/models/news_channel_headlines_model.dart';
+
+class NewsRepository {
+  Future<NewsChannelsHeadlinesModels> fetchNewsChannelHeadlineApi() async {
+    String url =
+        ' https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=c0cb83d6ac054269b5e2f4c397b898b9';
+
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final body = jsonDecode(response.body);
+      return NewsChannelsHeadlinesModels.fromJson(body);
+    }
+
+    throw Exception('Error');
+  }
+}
